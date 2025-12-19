f3834a68b9fbcab6094c939c9f369254ee717b60
Vinicius <vinicius.bruschi@catwork.com.br>
2025-12-18 22:49:00 -0400

    fix: composer permissions

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-php artisan schedule:work &
+echo "Fixing Laravel permissions..."
+
+mkdir -p storage bootstrap/cache
+
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
 
 exec docker-php-entrypoint php-fpm
